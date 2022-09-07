eeed32f8da2d8709d8a97c49a77f10013d52f11a
victorvictoria-maker <viktohrier01@gmail.com>
2022-09-07 15:33:48 +0100

    Countdown now working

--- js/main.js
+++ js/main.js
@@ -43,6 +43,29 @@
         });
     };
 
+    const countdown = () => {
+        const countDate = new Date('September 28, 2022 17:00:00').getTime();
+        const currentTime = new Date();
+        const gap = countDate - currentTime;
+
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+
+        const textDay = Math.floor(gap / day);
+        const textHour = Math.floor((gap % day)/hour);
+        const textMinute = Math.floor((gap % hour)/minute);
+        const textSecond = Math.floor((gap % minute)/second);
+
+        document.getElementById("textDay").innerHTML = textDay;
+        document.getElementById("textHour").innerHTML = textHour;
+        document.getElementById("textMinute").innerHTML = textMinute;
+        document.getElementById("textSecond").innerHTML = textSecond;
+    };
+
+    setInterval(countdown, 1000);
+
 
    /* Mobile Menu
     * ---------------------------------------------------- */ 
